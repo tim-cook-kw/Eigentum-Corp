0686d9efda64c19cf053e1b3b68ee51164170a1d
Haniffarhan <haniffarhan15@gmail.com>
2019-04-25 09:47:50 +0700

    Update Navbar Logo

--- resources/views/layouts/homelayouts.blade.php
+++ resources/views/layouts/homelayouts.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Home</title>
+    <title>Eigentum</title>
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i">
